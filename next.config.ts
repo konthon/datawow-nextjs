7125cdf2412be3b1f871fcae5a61c16b844fd936
konthon <konthon@hotmail.co.th>
2025-01-07 00:53:14 +0700

    chore: optimize bundle for Chakra UI lib

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx'],
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'],
+  },
 }
 
 export default nextConfig
