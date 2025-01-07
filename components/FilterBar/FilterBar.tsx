e814f49839c9267487ea1c8e74c8252c785e94f1
konthon <konthon@hotmail.co.th>
2025-01-08 00:50:42 +0700

    feat: handle filter bar mobile size

--- components/FilterBar/FilterBar.tsx
+++ components/FilterBar/FilterBar.tsx
@@ -1,6 +1,13 @@
-import { Button, createListCollection, HStack, Input } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  createListCollection,
+  HStack,
+  IconButton,
+  Input,
+} from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 import { communitiesQuery } from '@/queries/community'
 
@@ -17,6 +24,8 @@ import {
 import { usePostFilterStore } from './usePostFilter'
 
 const FilterBar: FC = () => {
+  const [openMobileSearch, toggleSearch] = useState(false)
+
   const onOpen = usePostDialogStore((state) => state.onOpen)
 
   const { data: communitiesData } = useQuery(communitiesQuery())
@@ -32,36 +41,55 @@ const FilterBar: FC = () => {
   )
 
   return (
-    <>
-      <HStack mb={{ base: 5, lg: 6 }} gap={{ base: 1, lg: 5 }}>
-        <InputGroup startElement={<SearchIcon />} flexGrow={1}>
-          <Input
-            placeholder='Search'
-            onChange={(e) => onTitleChange(e.target.value)}
-          />
-        </InputGroup>
-
-        <SelectRoot
-          width='128px'
-          collection={communities}
-          onValueChange={({ value }) => {
-            onCommunityChange(value?.[0] ? +value[0] : undefined)
+    <HStack mb={{ base: 5, lg: 6 }} gap={{ base: 1, lg: 5 }}>
+      <Box hideFrom='sm' flexGrow={1} hidden={openMobileSearch}>
+        <IconButton
+          aria-label='Search'
+          variant='plain'
+          onClick={() => {
+            toggleSearch(true)
           }}
         >
-          <SelectTrigger clearable>
-            <SelectValueText placeholder='Community' />
-          </SelectTrigger>
-          <SelectContent>
-            {communities.items.map((community) => (
-              <SelectItem key={community.id} item={community}>
-                {community.name}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </SelectRoot>
-        <Button onClick={() => onOpen()}>Create +</Button>
-      </HStack>
-    </>
+          <SearchIcon color='text' />
+        </IconButton>
+      </Box>
+
+      <InputGroup
+        startElement={<SearchIcon />}
+        flexGrow={1}
+        hideBelow={openMobileSearch ? undefined : 'sm'}
+      >
+        <Input
+          placeholder='Search'
+          onChange={(e) => onTitleChange(e.target.value)}
+          onBlur={() => toggleSearch(false)}
+        />
+      </InputGroup>
+
+      <SelectRoot
+        width='128px'
+        collection={communities}
+        onValueChange={({ value }) => {
+          onCommunityChange(value?.[0] ? +value[0] : undefined)
+        }}
+        hidden={openMobileSearch}
+      >
+        <SelectTrigger clearable>
+          <SelectValueText placeholder='Community' />
+        </SelectTrigger>
+        <SelectContent>
+          {communities.items.map((community) => (
+            <SelectItem key={community.id} item={community}>
+              {community.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </SelectRoot>
+
+      <Button onClick={() => onOpen()} hidden={openMobileSearch}>
+        Create +
+      </Button>
+    </HStack>
   )
 }
 
