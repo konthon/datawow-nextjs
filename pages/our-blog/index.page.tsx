cdbdcc9b793dd3dcfad6093f1bb4735b2cf378f9
konthon <konthon@hotmail.co.th>
2025-01-08 02:53:00 +0700

    feat: add our-blog page

---
+++ pages/our-blog/index.page.tsx
@@ -0,0 +1,61 @@
+import { Box, Stack } from '@chakra-ui/react'
+import { useQuery } from '@tanstack/react-query'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
+import { FilterBar, usePostFilter } from '@/components/FilterBar'
+import { Post } from '@/components/Post'
+import { PostDialog, usePostDialogStore } from '@/components/PostDialog'
+import { useAuth } from '@/hooks/useAuth'
+import { MainLayout } from '@/layouts/MainLayout'
+import { ourPostsQuery } from '@/queries/post'
+import { SIDEBAR_WIDTH } from '@/shared/navigation'
+
+const OurBlogPage: NextPage = () => {
+  const router = useRouter()
+  const { isLoggedIn, isLoading } = useAuth()
+
+  const postFilter = usePostFilter()
+  const { data } = useQuery(ourPostsQuery(postFilter))
+
+  const onOpenPost = usePostDialogStore((state) => state.onOpen)
+
+  useEffect(() => {
+    if (!isLoading && !isLoggedIn) {
+      router.push('/sign-in')
+    }
+  }, [router, isLoggedIn, isLoading])
+
+  return (
+    <>
+      <MainLayout backgroundColor='gray.100'>
+        <Box
+          mx={{ base: 4, lg: 10 }}
+          pr={SIDEBAR_WIDTH}
+          py={{ base: 12, lg: 8 }}
+        >
+          <FilterBar />
+          <Stack
+            as='section'
+            gap={0}
+            divideY='0.5px'
+            divideColor='gray.100'
+            borderRadius='xl'
+            overflow='hidden'
+          >
+            {data?.ourPosts.map((post) => (
+              <Link key={post.id} href={`/post/${post.id}`}>
+                <Post {...post} />
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+      </MainLayout>
+      <PostDialog />
+    </>
+  )
+}
+
+export default OurBlogPage
